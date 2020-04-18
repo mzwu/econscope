617ba6b5b98d438ee1b01e83ec039d195954ccd3
mzwu <55368740+mzwu@users.noreply.github.com>
2020-04-18 12:58:58 -0400

    update features

---
+++ econscope/data/fetch.py
@@ -0,0 +1,64 @@
+import requests
+import csv
+from econscope.utils.timer import Timer
+from econscope.data.proc import Proc
+
+_data_source = {
+    "nyt_us_states": {
+        "url": "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv",
+        "interval": 43200  # refreshes every 12 hours
+    },
+    "nyt_us_counties": {
+        "url": "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+        "interval": 43200  # refreshes every 12 hours
+    },
+    "xkey_test_data": {
+        "url": "https://iad.xkey.org/xwu/data/test.csv",
+        "interval": 10
+    }
+}
+
+
+def get(**kwargs):
+    """ get data from external URI """
+    target = kwargs.pop("target", None)
+    url = kwargs.pop("url", None)
+    ops = []
+    if url is not None:
+        return get_from_url(url)
+    if "__" in target:
+        tgts = target.split("__")
+        target = tgts[0]
+        ops = tgts[1:]
+    if target not in _data_source:
+        return {}
+    refresh = True
+    tgt = _data_source[target]
+    interval = tgt.get("interval", 43200)
+    ts_curr = Timer.get_epoch()
+    if "data" in tgt and "updated" in tgt and ts_curr - tgt["updated"] < interval:
+        refresh = False
+    if refresh:
+        tgt["data"] = get_from_url(tgt["url"])
+        if tgt["data"]:
+            tgt["updated"] = ts_curr
+    if ops:
+        data = Proc.process(target=target, data=tgt["data"], ops=ops)
+    else:
+        data = tgt["data"]
+    return data
+
+
+def get_from_url(url):
+    columns = {}
+    with requests.Session() as s:
+        dnld = s.get(url)
+        decoded_content = dnld.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        headers = next(cr, None)
+        for hdr in headers:
+            columns[hdr] = []
+        for row in cr:
+            for h, v in zip(headers, row):
+                columns[h].append(v)
+    return columns
--- econscope/web.py
+++ econscope/web.py
@@ -1,9 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, jsonify
+import os
+import econscope.data.data as econdata
+
+app = Flask(__name__)  # instantiate Flask with current name
+
+pwd = os.path.dirname(__file__)  # get directory name of current file
 
 
-app = Flask(__name__)
+def file_read(filename):
+    with open(os.path.join(pwd, filename), 'r') as fh:  # concatenate path + filename, fh=file handle
+        return fh.read()
+
+
+web_data = {
+    "topics": [
+        "unemployment",
+        "businesses",
+        "commodities",
+        "gdp",
+        "other"
+    ]
+}
 
 
 @app.route("/")
 def home_view():
-    return "<h1>It works</h1>"
+    return render_template('home.html', web_data=web_data)
+
+
+@app.route("/assets/<path:suburl>")
+def get_assets(suburl):
+    dirs = suburl.split("/")
+    dirname = "assets/" + "/".join(dirs[:-1])
+    return send_from_directory(dirname, dirs[-1])
+
+
+@app.route("/data/<string:target>")
+def data_view(target):
+    return jsonify(econdata.get(target=target))
+
+
+@app.route("/charts/<string:filename>")
+def chart_view(filename):
+    return render_template("charts/" + filename)
