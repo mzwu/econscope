a778404238715f7140f98497b3a55a2f0e6ce7b3
mzwu <55368740+mzwu@users.noreply.github.com>
2021-01-17 10:55:50 -0500

    weekly update #38

--- econscope/web.py
+++ econscope/web.py
@@ -140,6 +140,11 @@ def weekly_updates_april_2020():
     return render_template('updates/2020/updates2020april.html')
 
 
+@app.route('/updates-2021-0116')
+def update_01162021():
+    return render_template("updates/2021/01162021.html")
+
+
 @app.route('/updates-2021-0109')
 def update_01092021():
     return render_template("updates/2021/01092021.html")
